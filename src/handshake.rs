2e0b5613b6678567bbbe8afe250cfbc8dc01efe5
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-05-23 21:42:08 +0200

    update http-types
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/handshake.rs
+++ src/handshake.rs
@@ -1,7 +1,6 @@
 /// Upgrade an HTTP connection into an SSE session.
-pub fn upgrade(headers: &mut impl AsMut<http_types::Headers>) -> http_types::Result<()> {
+pub fn upgrade(headers: &mut impl AsMut<http_types::Headers>) {
     let headers = headers.as_mut();
-    headers.insert("Cache-Control", "no-cache")?;
-    headers.insert("Content-Type", "text/event-stream")?;
-    Ok(())
+    headers.insert("Cache-Control", "no-cache");
+    headers.insert("Content-Type", "text/event-stream");
 }
