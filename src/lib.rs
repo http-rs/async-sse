7f26b41ea735acdaf58aede3f868c24e988d5dbd
Yoshua Wuyts <yoshuawuyts@gmail.com>
2020-09-29 11:18:25 +0200

    Fix pin-project issues
    See https://github.com/taiki-e/pin-project-lite/issues/33

--- src/lib.rs
+++ src/lib.rs
@@ -31,9 +31,8 @@
 //! - [SSE Spec](https://html.spec.whatwg.org/multipage/server-sent-events.html#concept-event-stream-last-event-id)
 //! - [EventSource web platform tests](https://github.com/web-platform-tests/wpt/tree/master/eventsource)
 
-#![forbid(rust_2018_idioms)]
 #![deny(missing_debug_implementations, nonstandard_style)]
-#![warn(missing_docs, missing_doc_code_examples, unreachable_pub)]
+#![warn(missing_docs, rust_2018_idioms, unreachable_pub)]
 
 mod decoder;
 mod encoder;
